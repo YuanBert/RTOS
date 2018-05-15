3a07bfebb895a8ffe32e68efa32409432c084580
Bert <zgenyuan@hotmail.com>
2018-05-15 18:05:50 +0800

    添加资源锁代码

---
+++ 07/src/tinyOS.h
@@ -0,0 +1,26 @@
+#ifndef __tinyOS_H
+#define __tinyOS_H
+
+#include <stdint.h>
+
+//Cortex-M 的栈单元类型：堆栈单元的大小为32位
+typedef uint32_t tTaskStack;
+
+//任务结构：包含了把一任务的所有信息
+typedef struct _tTask{
+	
+	uint32_t * stack;
+	
+	//任务延时计数器
+	uint32_t delayTicks;
+}tTask;
+
+extern tTask *currentTask;
+extern tTask *nextTask;
+
+void  tTaskSwitch(void);
+void  tTaskRunFirst(void);
+uint32_t tTaskEnterCritical(void);
+void tTaskExitCritical(uint32_t status);
+
+#endif
